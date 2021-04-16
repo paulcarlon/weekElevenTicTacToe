a8469e4d9ca0f86b1c570d2ab85685b78c1ba0ca
paulcarlon <paulcarlonlv@gmail.com>
2021-04-16 08:52:37 -0700

    added comments for ease of understanding

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
+//Initialize empty arrays to hold box numbers each player has
 let xCards = [];
 let oCards = [];
+//Initialize 2 players and their symbols
 const playerOne = '◯';
 const playerTwo = 'X';
+//Define initial players turn
 let playersTurn = playerOne;
+//Initialize array to keep track of how many boxes are filled
 let filledSpaces = [];
+//Grab elements in HTML document for displaying winner and whose turn it is
 const turnDisplay = document.getElementById('turnDisplay');
 const winnerDisplay = document.getElementById('winnerDisplay');
-//Add listener for a click on each of the boxes
+//Grab all of the box divs
 const boxes = document.querySelectorAll('.boxes');
+//Grab onto h3 for changing turn text
 let turnAlert = document.getElementById('turnAlert');
+//Add listener for a click on each of the boxes
 boxes.forEach((box) => {
   box.addEventListener('click', () => {
+    //add each space filled to filledSpaces array
     filledSpaces.push(box);
-    console.log(filledSpaces.length);
+    //define that if all spaces are filled and no one has won the game is a draw and display that in DOM
     if (filledSpaces.length == 9) {
       openModal();
       modalHeader.innerHTML = "<h4>It's a Draw!</h4>";
     }
+    //add o to box clicked if o is current player
     if (playersTurn === playerOne) {
       let playerOneClick = document.createElement('h1');
-      playerOneClick.style.fontSize = '150px';
+      playerOneClick.style.fontSize = '100px';
       playerOneClick.style.color = 'green';
+      playerOneClick.style.transform = 'translateY(22%)';
       playerOneClick.classList =
         'd-flex justify-content-center align-items-center text-center';
-      playerOneClick.innerText = '◯';
+      playerOneClick.innerText = playerOne;
       box.appendChild(playerOneClick);
       oCards.push(box.id);
       playersTurn = playerTwo;
       turnAlert.innerHTML =
         "It's player <span style='color: darkblue; text-shadow: 0px 0px 6px #fff;'>✕</span>'s turn!";
-
+      //check for o winning combinations
       if (
         (oCards.includes('1') &&
           oCards.includes('2') &&
@@ -58,11 +68,13 @@ boxes.forEach((box) => {
         modalHeader.innerHTML =
           "<h4>Congratulations, Player <span class='h3' style='color: green; text-shadow: 0px 0px 6px #fff;'>◯</h5><h3>!</h3>";
       }
+      //add x to box clicked if x is current player
     } else if (playersTurn === playerTwo) {
       let playerTwoClick = document.createElement('h1');
-      playerTwoClick.innerText = '✕';
-      playerTwoClick.style.fontSize = '150px';
+      playerTwoClick.innerText = playerTwo;
+      playerTwoClick.style.fontSize = '100px';
       playerTwoClick.style.color = 'darkblue';
+      playerTwoClick.style.transform = 'translateY(22%)';
       playerTwoClick.classList =
         'd-flex justify-content-center align-items-center text-center';
       box.appendChild(playerTwoClick);
@@ -70,7 +82,7 @@ boxes.forEach((box) => {
       playersTurn = playerOne;
       turnAlert.innerHTML =
         "It's player <span class='h3' style='color: green; text-shadow: 0px 0px 6px #fff;'>◯</span>'s turn!";
-
+      //check for x winning combinations
       if (
         (xCards.includes('1') &&
           xCards.includes('2') &&
@@ -102,9 +114,8 @@ boxes.forEach((box) => {
     }
   });
 });
-
+//define modal display for when game has ended
 let modalHeader = document.querySelector('.modal-header');
-
 let ticTacModal = document.getElementById('ticTacModal');
 let restartBtn = document.getElementById('restartBtn');
 let mainRestart = document.getElementById('mainRestart');
@@ -114,11 +125,12 @@ function openModal() {
   ticTacModal.classList.add('show');
   turnDisplay.style.display = 'none';
 }
-
+//add 'click' listener to the always-displayed restart button and the restart button that appears in the modal when the game has ended
 restartBtn.addEventListener('click', freshSlate);
 
 mainRestart.addEventListener('click', freshSlate);
 
+//function for game restart
 function freshSlate() {
   oCards = [];
   xCards = [];
